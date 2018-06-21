4dfa9ecf1abce7d21003c3947a99dccc59f6720f
Thomas Dehaeze <thomas.dehaeze@esrf.fr>
2018-06-21 11:03:52 +0200

    Use subaxis instead of subplot. Add frequency argument.

--- src/bodeFig.m
+++ src/bodeFig.m
@@ -1,85 +1,97 @@
 function [] = bodeFig(systems, freqs, opts_param)
+    %% Default values for opts
+    opts = struct(...
+        'phase', false, ...
+        'ylabel', '' ...
+    );
 
-%% Default values for opts
-opts = struct('phase', false);
+    %% Check if the frequency is specified
+    ni = nargin;
 
-%% Check if the frequency is specified
-ni = nargin;
+    if ni == 2
+        if isstruct(freqs)
+            opts_param = freqs;
+            clear freqs;
+        else
+            opts_param = struct();
+        end
+    end
 
-if ni == 2
-    if isstruct(freqs)
-        opts_param = freqs;
-        clear freqs;
+    %% Populate opts with input parameters
+    if exist('opts_param','var')
+        for opt = fieldnames(opts_param)'
+            opts.(opt{1}) = opts_param.(opt{1});
+        end
+    end
+
+    %% Number of subplots
+    n_subplots = 1;
+    if opts.phase
+        n_subplots = 2;
+    end
+
+    %% Cell of transfer function to array of tf
+    systems_arr = tf(0)*zeros(length(systems), 1);
+    for i = 1:length(systems)
+        systems{i}.InputName = {};
+        systems{i}.OutputName = {};
+        systems_arr(i) = tf(systems{i});
+    end
+
+    %% Compute the frequency response
+    bode_opts = bodeoptions;
+    bode_opts.FreqUnits       = 'Hz';
+    bode_opts.PhaseWrapping   = 'on';
+
+    if ~exist('freqs', 'var')
+        [resp,freqs] = freqresp(systems_arr);
+        freqs = freqs/(2*pi);
     else
-        opts_param = struct();
+        [resp,~] = freqresp(systems_arr, freqs, 'Hz');
     end
-end
+    resp_mag = abs(resp);
+    resp_phase = 180/pi*angle(resp);
 
-%% Populate opts with input parameters
-if exist('opts_param','var')
-    for opt = fieldnames(opts_param)'
-        opts.(opt{1}) = opts_param.(opt{1});
+    %% Plot
+    figure;
+
+    %% Phase
+    if opts.phase
+        ax2 = subaxis(n_subplots,1,2);
+        hold on;
+        for i = 1:length(systems)
+            plot(freqs, mod(180+resp_phase(i, :), 360)-180);
+        end
+        set(gca,'xscale','log');
+        ylim([-180, 180]);
+        yticks([-180, -90, 0, 90, 180]);
+        xlabel('Frequency [Hz]'); ylabel('Phase [deg]');
+        hold off;
     end
-end
 
-%% Number of subplots
-n_subplots = 1;
-if opts.phase
-    n_subplots = 2;
-end
-
-%% Cell of transfer function to array of tf
-systems_arr = tf(0)*zeros(length(systems), 1);
-for i = 1:length(systems)
-    systems{i}.InputName = {};
-    systems{i}.OutputName = {};
-    systems_arr(i) = systems{i};
-end
-
-%% Compute the frequency response
-bode_opts = bodeoptions;
-bode_opts.FreqUnits       = 'Hz';
-bode_opts.PhaseWrapping   = 'on';
-
-if ~exist('freqs', 'var')
-    [resp_mag,resp_phase,freqs] = bode(systems_arr, bode_opts);
-else
-    [resp_mag,resp_phase,freqs] = bode(systems_arr, freqs, bode_opts);
-end
-
-%% Plot
-figure;
-
-%% Phase
-if opts.phase
-    ax2 = subplot(n_subplots,1,2);
+    %% Amplitude
+    ax1 = subaxis(n_subplots,1,1);
     hold on;
     for i = 1:length(systems)
-        plot(freqs, mod(180+resp_phase(i, :), 360)-180);
+        plot(freqs, resp_mag(i, :));
     end
-    set(gca,'xscale','log');
-    ylim([-180, 180]);
-    yticks([-180, -90, 0, 90, 180]);
-    xlabel('Frequency ($Hz$)'); ylabel('Phase (deg)');
+    set(gca,'xscale','log'); set(gca,'yscale','log');
+    if length(opts.ylabel) > 1
+        ylabel(opts.ylabel);
+    else
+        ylabel('Amplitude');
+    end
+    if ~opts.phase
+        xlabel('Frequency [Hz]');
+    else
+        set(ax1,'XTickLabel',[]);
+    end
     hold off;
+
+    %% Link X-axis
+    if opts.phase
+        linkaxes([ax1, ax2], 'x');
+    end
+
+    xlim([freqs(1), freqs(end)]);
 end
-
-%% Amplitude
-ax1 = subplot(n_subplots,1,1);
-hold on;
-for i = 1:length(systems)
-    plot(freqs, resp_mag(i, :));
-end
-set(gca,'xscale','log'); set(gca,'yscale','log');
-ylabel('Amplitude');
-
-xlabel('Frequency ($Hz$)');
-hold off;
-
-%% Link X-axis
-if opts.phase
-	linkaxes([ax1, ax2], 'x');  
-end
-
-end
-
